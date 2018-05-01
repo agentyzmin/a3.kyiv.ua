643557e3d04e88005dfce976abcd95bf675a8855
Alex Kolodko <alexkolodko@gmail.com>
2018-05-01 12:58:27 +0300

    May 1, 2018

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// var gulp        = require('gulp');
+// var browserSync = require('browser-sync').create();
+// var reload      = browserSync.reload;
+
+// // Save a reference to the `reload` method
+
+// // Watch scss AND html files, doing different things with each.
+// gulp.task('serve', function () {
+
+//     // Serve files from the root of this project
+//     browserSync.init({
+//         server: {
+//             baseDir: "./"
+//         }
+//     });
+
+//     gulp.watch("*.html").on("change", reload);
+// });
+
+
+
+
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+  
+gulp.task('browser-sync', function () {
+  var files = [
+    '**/*',
+    // 'css/**/*.css',
+    // 'i**/*.png',
+    // '**/assets/*'
+  ];
+  
+  browserSync.init(files, {
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
